405def08495ed394cc9cfadd3facc5ec69b42c2c
Darius Navickas <dariusnaw@gmail.com>
2023-02-26 02:08:26 +0200

    edited responsive form mobile

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const sr = ScrollReveal({
 sr.reveal('.home-text', { delay: 350, origin: 'left' });
 sr.reveal('.home-img', { delay: 350, origin: 'right' });
 
-sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact', {
+sr.reveal('.sub-service,.skills,.about,.portfolio,.service,.cta,.contact', {
 	delay: 200,
 	origin: 'bottom',
 });
